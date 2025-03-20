c1defe60ff33e16095bdbcb9f717f98968af35f6
Kenneth Griesser <kgriesser@tclcomplabs.com>
2025-03-20 15:03:43 -0700

    changed test for --output-directory to reflect it will only pass on devel branch of sst. Added some basic checks for creating differently formatted directories. Also uniquified the directory name to avoid name collisions with parallel test runs

---
+++ tests/cli/sst_output_dir-DEV.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#EXT_TEST TEST_FILE_PARAM ALL
+#EXT_TEST TEST_FILE_DESC "Tests directory creation using --output-directory"
+#EXT_TEST DEP "coreTestElement.coreTestComponent"
+
+check_odir() {
+    odir=$1
+    rm -Rf $odir
+    sst --output-directory=$odir cli-sdl.py
+    retVal=$?
+    if [ $retVal -ne 0 ]; then
+	echo "ERROR : $retVal"
+	exit $retVal
+    fi
+    if [ ! -d "$odir" ]; then
+	echo "ERROR : Output directory ${odir} does not exist!"
+	exit 1
+    fi
+    rm -Rf $odir
+    echo "${odir} OK"
+}
+
+# Check 1: 1 level subdirectory
+check_odir sst_output_dir.$$
+
+# Check 2: 1 level subdirectory with ./
+check_odir ./sst_output_dir.$$
+
+# Check 3: Absolute path
+check_odir "$(realpath .)/sst_output_dir.$$"
+
+# All good
+echo "PASS"
+exit 0
