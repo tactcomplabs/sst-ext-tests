2916ba25fdaaac99d3d21c058d77d774c92a71fb
Kenneth Griesser <kgriesser@tclcomplabs.com>
2025-04-03 14:50:12 -0700

    update heartbeat tests. Now just checking CLI

--- tests/cli/sst_heartbeat_90s-DEV.sh
+++ tests/cli/sst_heartbeat_90s-DEV.sh
@@ -3,10 +3,11 @@
 #EXT_TEST TEST_FILE_DESC "Tests a 90 second heartbeat using seconds-only syntax"
 #EXT_TEST DEP "coreTestElement.coreTestComponent"
 
-# selecting count to run for a few seconds.
-# A test component that has ability to specify its runtime in seconds would be more portable
 log=sst_hearbeat_90s.log
-sst --heartbeat-wall-period=90s cli-sdl.py -- --count=7000000 > $log
+
+# Just check the command-line parsing
+# sst --heartbeat-wall-period=90s --stop-at=1000us cli-sdl.py > $log
+sst --heartbeat-wall-period=90s cli-sdl.py > $log
 
 retVal=$?
 if [ $retVal -ne 0 ]; then
@@ -15,11 +16,11 @@ if [ $retVal -ne 0 ]; then
 fi
 
 # Check heartbeat actually happened
-grep "Simulation Heartbeat:" $log
-if [ $? -ne 0 ]; then
-  echo "ERROR: no heartbeat"
-  exit 1
-fi
+# grep "Simulation Heartbeat:" $log
+# if [ $? -ne 0 ]; then
+#   echo "ERROR: no heartbeat"
+#   exit 1
+# fi
 
 # passed: clean up
 rm -f $log
