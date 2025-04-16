843a9fc568a2f777acca2b869f7469161f718099
John Leidel <john.leidel@gmail.com>
2025-04-16 16:45:56 -0500

    adding support for forthcoming sst 15.0 release

---
+++ tests/cli/sst_help-15.0.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#EXT_TEST TEST_FILE_PARAM 15.0
+#EXT_TEST TEST_FILE_DESC "Displays the help menu of SST 15.0"
+
+sst --help
+
+retVal=$?
+if [ $retVal -ne 0 ]; then
+  echo "ERROR"
+  exit $retVal
+fi
+
+sst --help=verbose
+
+retVal=$?
+if [ $retVal -ne 0 ]; then
+  echo "ERROR"
+  exit $retVal
+fi
+
+echo "PASS"
+exit $retVal
