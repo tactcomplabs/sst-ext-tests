ed18bf423526a2b911ae5ac838accde7d3ad5d38
John Leidel <john.leidel@gmail.com>
2025-05-13 11:06:54 -0500

    improving captcrunch scripts

--- tests/component_tests/CaptCrunch_Simple1-DEV.sh
+++ tests/component_tests/CaptCrunch_Simple1-DEV.sh
@@ -4,6 +4,7 @@
 
 TEST_NAME=CaptCrunch_Simple1-DEV
 
+rm -Rf $TEST_NAME.py
 cat > $TEST_NAME.py << EOL
 import sst
 sst.setStatisticLoadLevel(4)
@@ -18,7 +19,7 @@ c0.addParams({
 EOL
 
 # -- run the first pass through the sim
-sst --checkpoint-period=10ns --checkpoint-prefix=$TEST_NAME-PRE --add-lib-path=${CMAKE_BINARY_DIR}/tests/components/CaptCrunch/ CaptCrunch_Simple1-DEV.py
+sst --checkpoint-period=10ns --checkpoint-prefix=$TEST_NAME-PRE --add-lib-path=${CMAKE_BINARY_DIR}/tests/components/CaptCrunch/ $TEST_NAME.py
 
 retVal=$?
 if [ $retVal -ne 0 ]; then
