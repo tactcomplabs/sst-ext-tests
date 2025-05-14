7cc7bb6fa97801f098737e4702a0d27b35632ea1
John Leidel <john.leidel@gmail.com>
2025-05-14 11:20:41 -0500

    fixing environment switch for captcrunch tests

--- tests/component_tests/CaptCrunch_Simple1-MIN15.0.sh
+++ tests/component_tests/CaptCrunch_Simple1-MIN15.0.sh
@@ -19,7 +19,7 @@ c0.addParams({
 EOL
 
 # -- run the first pass through the sim
-sst --checkpoint-period=10ns --checkpoint-prefix=$TEST_NAME-PRE --add-lib-path=${SST_COMPONENT_BASE}/tests/components/CaptCrunch/ $TEST_NAME.py
+sst --checkpoint-period=10ns --checkpoint-prefix=$TEST_NAME-PRE --add-lib-path=$SST_COMPONENT_BASE/tests/components/CaptCrunch/ $TEST_NAME.py
 
 retVal=$?
 if [ $retVal -ne 0 ]; then
@@ -30,7 +30,7 @@ fi
 CPTFILE=`find ./$TEST_NAME-PRE -name "$TEST_NAME-PRE_9_*" | grep sstcpt`
 
 # -- restart the sim from a known good checkpoint
-sst --load-checkpoint --checkpoint-period=10ns --checkpoint-prefix=$TEST_NAME-POST --add-lib-path=${SST_COMPONENT_BASE}/tests/components/CaptCrunch/ $CPTFILE
+sst --load-checkpoint --checkpoint-period=10ns --checkpoint-prefix=$TEST_NAME-POST --add-lib-path=$SST_COMPONENT_BASE/tests/components/CaptCrunch/ $CPTFILE
 
 retVal=$?
 
