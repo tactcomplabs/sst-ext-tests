455024b8c945c243af7db841457666c2eebab7ed
Kenneth Griesser <kgriesser@tclcomplabs.com>
2025-09-21 12:07:55 -0700

    debug console logging and replay sanity tests

--- tests/core-debug/cmd-replay-DEV.sh
+++ tests/core-debug/cmd-replay-DEV.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #EXT_TEST TEST_FILE_PARAM DEV
-#EXT_TEST TEST_FILE_DESC "Enter interactive mode and log commands to an external file"
+#EXT_TEST TEST_FILE_DESC "Enter interactive mode and replay session from an external file"
 #EXT_TEST TIMEOUT 30
 #
 # SST DEV: 
 
 # Settings
 CLEANUP=1
-TNAME=cmd-logging-DEV
+TNAME=cmd-replay-DEV
 CONFIG="../rt_action/test_Checkpoint.py"
 PSTR="c7 finished. teststring=HelloMyNameIsC7AndICannotQuoteAString"
 
@@ -17,7 +17,6 @@ CMDFILE=$TNAME.cmd
 CHKFILE=$TNAME.chk
 
 cat << EOF > $CMDFILE
-logging $OUTFILE
 ls
 cd c7
 ls
@@ -29,7 +28,9 @@ EOF
 # Launch the program to start interactive mode at time 0
 LAUNCH="sst --interactive-console=sst.interactive.simpledebug --interactive-start=0s $CONFIG"
 echo $LAUNCH
-$LAUNCH < $CMDFILE > $LOGFILE || exit 1
+$LAUNCH << EOF | tee $LOGFILE || exit 1
+replay $CMDFILE
+EOF
 
 wait
 retVal=$?
@@ -39,15 +40,6 @@ echo $TNAME Complete
 # Check result
 if [ $retVal -ne 0 ]; then
   echo "ERROR $NAME returned $retVal"
-  exit $retVal
-fi
-
-# Should match except for the first 'logging' line
-grep -v logging $CMDFILE > $CHKFILE
-diff -q $CHKFILE $OUTFILE
-retVal=$?
-if [ $retVal -ne 0 ]; then
-  echo "ERROR console output does not match expected result"
   exit $retVal
 fi
 
