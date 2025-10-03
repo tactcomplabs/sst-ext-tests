6849bf2ea40333e68027a613aebfede6e3cc7d38
Shannon Kuntz <skuntz@tactcomplabs.com>
2025-10-03 14:33:52 -0500

    More tests for cmd-line changes

---
+++ tests/core-debug/cmd-ckptaction-err-DEV.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#EXT_TEST TEST_FILE_PARAM DEV
+#EXT_TEST TEST_FILE_DESC "Check that trace checkpoint action triggers checkpoints"
+#EXT_TEST TIMEOUT 30
+#
+# SST DEV: 
+
+# Settings
+CLEANUP=1
+SCRIPT_NAME=$(basename "$0")
+TNAME="${SCRIPT_NAME%.*}"
+echo "TESTNAME=$TNAME"
+CONFIG="test_Checkpoint_4ms.py"
+PSTR=""
+
+LOGFILE=$TNAME.log
+OUTFILE=$TNAME.console.out
+CMDFILE=$TNAME.cmd
+CHKFILE=$TNAME.chk
+CKPTPREFIX=ckpt_$TNAME
+
+# Launch the program to start interactive mode at time 0
+LAUNCH="sst --interactive-start=0s --checkpoint-prefix=$CKPTPREFIX $CONFIG"
+echo $LAUNCH
+$LAUNCH << EOF | tee $LOGFILE || exit 1
+cd c0
+cd xorshift
+trace w changed : 32 4 : w x y z : checkpoint
+shutdown
+EOF
+
+retVal=$?
+
+echo $TNAME Complete
+
+# Check result
+if [ $retVal -ne 0 ]; then
+  echo "ERROR $NAME returned $retVal"
+  exit $retVal
+fi
+
+PSTR="Invalid action: checkpointing not enabled"
+grep "$PSTR" $LOGFILE > /dev/null
+retVal=$?
+if [ $retVal -ne 0 ]; then
+  echo "ERROR could not find pass string in $LOGFILE \"$PSTR\""
+  exit $retVal
+fi
+echo "Found pass string \"$PSTR\""
+
+# Simulation Complete
+PSTR="Simulation is complete"
+grep "$PSTR" $LOGFILE > /dev/null
+retVal=$?
+if [ $retVal -ne 0 ]; then
+  echo "ERROR could not find pass string in $LOGFILE \"$PSTR\""
+  exit $retVal
+fi
+echo "Found pass string \"$PSTR\""
+
+if [ $CLEANUP -eq 1 ]; then
+  rm -f $LOGFILE $OUTFILE $CMDFILE $CHKFILE
+  rm -rf $CKPTPREFIX
+fi
+
+wait
+echo "PASS"
+exit $retVal
