bb11dbd1463a407336dc505e7a5605d8223769d4
Kenneth Griesser <kgriesser@tclcomplabs.com>
2025-11-24 09:42:23 -0700

    safer more verbose heartbeat test

--- tests/cli/sst_heartbeat_1s.sh
+++ tests/cli/sst_heartbeat_1s.sh
@@ -3,10 +3,14 @@
 #EXT_TEST TEST_FILE_DESC "Tests a 1 second heartbeat"
 #EXT_TEST DEP "coreTestElement.coreTestComponent"
 
+
+# ensure non-zero exit code in pipe propagates and no unbound variables.
+set -uo pipefail
+
 # selecting count to run for a few seconds.
 # A test component that has ability to specify its runtime in seconds would be more portable
 log=sst_hearbeat_1s.log
-sst --heartbeat-wall-period=1s cli-sdl.py -- --count=200000 > $log
+sst --heartbeat-wall-period=1s cli-sdl.py -- --count=200000 | tee $log
 
 retVal=$?
 if [ $retVal -ne 0 ]; then
