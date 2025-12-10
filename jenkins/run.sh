d90bf92d6b0be4161d6918df6ffcf9df6bf0285a
Kenneth Griesser <kgriesser@tclcomplabs.com>
2025-12-10 16:22:25 -0700

    .

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -47,6 +47,7 @@ echo "VALGRIND=$VALGRIND"
 #-- SST
 rm -Rf $SST_INSTALL/*
 if [ "$CLANGFORMAT" = true ]; then
+	ls
 	./scripts/clang-format-test.sh --format-exe "${CLANG_FORMAT_EXE}"
 fi
 ./autogen.sh
