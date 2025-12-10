0912c659ff806df9f4361e4718d25cc5003af170
Ken Griesser <kgriesser@tactcomplabs.com>
2025-12-10 23:49:07 +0000

    Revert "ignore sst-ext-bench for clang-format test"
    This reverts commit 3d4c66213abb9b88ee1bcc3326f077259f0eafc0.

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -47,7 +47,7 @@ echo "VALGRIND=$VALGRIND"
 #-- SST
 rm -Rf $SST_INSTALL/*
 if [ "$CLANGFORMAT" = true ]; then
-	echo "sst-ext-bench/" >> .clang-format-ignore
+	ls
 	./scripts/clang-format-test.sh --format-exe "${CLANG_FORMAT_EXE}"
 fi
 ./autogen.sh
