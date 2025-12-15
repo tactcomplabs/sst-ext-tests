ddcf39c5fc81320e64563042b696040e06d1f739
Kenneth Griesser <kgriesser@tclcomplabs.com>
2025-12-15 14:21:46 -0700

    changed min version of sigalrm_combined to 15.1

--- tests/rt_action/sigalrm_combined.sh
+++ tests/rt_action/sigalrm_combined.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#EXT_TEST TEST_FILE_MINVER 14.1
+#EXT_TEST TEST_FILE_MINVER 15.1
 #EXT_TEST TEST_FILE_DESC "Tests sigalrm for combinations of two real time actions (checkpoint not included)"
 #EXT_TEST TIMEOUT 90
 # 
