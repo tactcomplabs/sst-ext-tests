9bbbe3082886150694197bfd31e158d316dae7b9
Kenneth Griesser <kgriesser@tclcomplabs.com>
2025-12-22 11:44:52 -0700

    added check for unmapped union objects

--- tests/core-debug/type-aggregate.sh
+++ tests/core-debug/type-aggregate.sh
@@ -10,8 +10,17 @@ set -uo pipefail
 # Set default ensure failure if not set and component not installed
 SST_COMPONENT_BASE="${SST_COMPONENT_BASE:=.}"
 
-# Settings
-CLEANUP=1
+# Convenience environment variables
+set +u
+if [[ -z "${CLEANUP}" ]]; then
+  CLEANUP=1
+fi
+if [[ -z "${VERBOSE}" ]]; then
+  VERBOSE=0
+fi
+set -u
+
+# Common settings
 SCRIPT_PATH="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 SCRIPT_NAME=$(basename "$0")
 TNAME="${SCRIPT_NAME%.*}"
@@ -45,68 +54,68 @@ p v_ag_class
 # CHECK 1 p v_ag_struct\nv_ag_struct \(SSTDEBUG::DbgSST15::ag_struct_t\)\n 0 = 42 \(.+\n 1 = ag_struct_t \(
 p v_ag_struct
 
-#TODO v_ag_union
-#TODO p v_ag_union_struct
+# Trivially serializable types do not automatically map
+# CHECK 2 p v_ag_union\nUnknown object in print command: v_ag_union
+p v_ag_union
+
+# CHECK 3 p v_ag_union_struct\nUnknown object in print command: v_ag_union_struct
+p v_ag_union_struct
 
 #-- Navigate into objects, print, set and check values
 
 cd v_ag_class
-# CHECK 2 ls\n0 = 42 \(.+\n1 = ag_class_t \(
+# CHECK 4 ls\n0 = 42 \(.+\n1 = ag_class_t \(
 ls
 set 0 100
 set 1 howdy_class
-# CHECK 3 ls\n0 = 100 \(.+\n1 = howdy_class \(
+# CHECK 5 ls\n0 = 100 \(.+\n1 = howdy_class \(
 ls
 
 cd ..
 cd v_ag_struct
-# CHECK 4 ls\n0 = 42 \(.+\n1 = ag_struct_t \(
+# CHECK 6 ls\n0 = 42 \(.+\n1 = ag_struct_t \(
 ls
 set 0 200
 set 1 howdy_struct
-# CHECK 5 ls\n0 = 200 \(.+\n1 = howdy_struct \(
+# CHECK 7 ls\n0 = 200 \(.+\n1 = howdy_struct \(
 ls
 
 cd ..
-#TODO v_ag_union
-#TODO p v_ag_union_struct
 
 ### Set watchpoints and run
 
 cd v_ag_class
 watch 0 changed
-# CHECK 6 setHandler 0 ac\nWP 0 - cp0/v_ag_class/0
+# CHECK 8 setHandler 0 ac\nWP 0 - cp0/v_ag_class/0
 setHandler 0 ac
 run
-# CHECK 7 ls\n0 = 101 \(.+\n1 = 19 \(
+# CHECK 9 ls\n0 = 101 \(.+\n1 = 19 \(
 ls
 run
-# CHECK 8 ls\n0 = 102 \(.+\n1 = 38 \(
+# CHECK 10 ls\n0 = 102 \(.+\n1 = 38 \(
 ls
 run
-# CHECK 9 ls\n0 = 103 \(.+\n1 = 57 \(
+# CHECK 11 ls\n0 = 103 \(.+\n1 = 57 \(
 ls
 unwatch
 
 cd ..
 cd v_ag_struct
 watch 0 changed
-# CHECK 10 setHandler 0 ac\nWP 0 - cp0/v_ag_struct/0
+# CHECK 12 setHandler 0 ac\nWP 0 - cp0/v_ag_struct/0
 setHandler 0 ac
 run
-# CHECK 11 ls\n0 = 203 \(.+\n1 = 69 \(
+# CHECK 13 ls\n0 = 203 \(.+\n1 = 69 \(
 ls
 run
-# CHECK 12 ls\n0 = 204 \(.+\n1 = 92 \(
+# CHECK 14 ls\n0 = 204 \(.+\n1 = 92 \(
 ls
 run
-# CHECK 13 ls\n0 = 205 \(.+\n1 = 115 \(
+# CHECK 15 ls\n0 = 205 \(.+\n1 = 115 \(
 ls
 unwatch
 
 cd ..
-#TODO v_ag_union
-#TODO p v_ag_union_struct
 
 #TODO Traces
 
@@ -115,10 +124,10 @@ shutdown
 EOF
 
 # Update this whenever adding checks in the command comments above
-NUMCHECKS=14
+NUMCHECKS=16
 
 # Launch the program to start interactive mode at time 0
-LAUNCH="sst --interactive-start=0s --add-lib-path=$SST_COMPONENT_BASE/core-debug $CONFIG -- --verbose=0"
+LAUNCH="sst --interactive-start=0s --add-lib-path=$SST_COMPONENT_BASE/core-debug --verbose=$VERBOSE $CONFIG"
 echo $LAUNCH
 revVal=0
 $LAUNCH << EOF  | tee $LOGFILE
