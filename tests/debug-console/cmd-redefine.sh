64c651e8c9ee6b63a3af441271e351fd95b1e844
Shannon Kuntz <skuntz@tactcomplabs.com>
2026-05-19 12:17:05 -0500

    Update user-defined command tests for latest changes

---
+++ tests/debug-console/cmd-redefine.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+#EXT_TEST TEST_FILE_MINVER NEW
+#EXT_TEST TEST_FILE_DESC "Verify rankserial re-defined commands: 2 ranks, 1 thread/rank"
+#EXT_TEST TIMEOUT 30
+
+# ensure non-zero exit code in pipe propagates and no unbound variables.
+set -uo pipefail
+
+# --add-lib-path required for Jenkins runs (does not run 'make install')
+# Set default ensure failure if not set and component not installed
+SST_COMPONENT_BASE="${SST_COMPONENT_BASE:=.}"
+
+# Settings
+CLEANUP=1
+SCRIPT_PATH="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+SCRIPT_NAME=$(basename "$0")
+TNAME="${SCRIPT_NAME%.*}"
+echo "TESTNAME=$TNAME"
+CONFIG=$(realpath dbgsst15.py)
+RANKS=2
+THREADS=1
+
+OS_TYPE=$(uname -s)
+MPIOPTS=""
+if [ ${OS_TYPE} = "Linux" ]; then
+  MPIOPTS="--bind-to socket"
+fi
+
+LOGFILE=$TNAME.log
+OUTFILE=$TNAME.console.out
+CMDFILE=$TNAME.cmd
+CHKFILE=$TNAME.chk
+SPOTCHECKS=$(realpath "${SCRIPT_PATH}/../../scripts/spotchecks.awk")
+if [ ! -e "${SPOTCHECKS}" ]; then
+  echo "Checker script not found. [${SPOTCHECKS}]"
+  exit 1
+fi
+
+cat << EOF > $CMDFILE
+confirm false
+
+# The checking convention is: CHECK <id> <regexp>
+# regexp and have multiple \n characters but not a trailing \n
+
+
+# CHECK 0 define fubar\nEnter commands for "fubar" terminated by "end"
+define fubar
+cd cp0
+print size
+cd ..
+end
+
+
+# CHECK 1 fubar\nsize = 90
+fubar
+
+# CHECK 2 define foo\nEnter commands for "foo" terminated by "end"
+define foo
+ls
+end
+
+# CHECK 3 show fubar\nUser command "fubar":\n.+cd cp0\n.+print size\n.+cd ..
+show fubar
+
+# CHECK 4 show foo\nUser command "foo":\n\tls
+show foo
+
+# CHECK 5 show\n--- User-Defined Commands ---\nUser command "fubar":\n.+cd cp0\n.+print size\n.+cd ..\n\nUser command "foo":\n\tls
+show
+
+shutdown
+EOF
+
+# Update this whenever adding checks in the command comments above
+NUMCHECKS=6
+
+# Launch the program to start interactive mode at time 0
+LAUNCH="mpirun ${MPIOPTS} -np $RANKS sst -n $THREADS --interactive-start=0s --add-lib-path=$SST_COMPONENT_BASE/core-debug $CONFIG -- --verbose=0"
+echo $LAUNCH
+retVal=0
+$LAUNCH << EOF  | tee $LOGFILE
+replay $CMDFILE
+confirm false
+exit
+EOF
+
+retVal=$?
+
+echo $TNAME Complete
+
+# Check result
+if [ $retVal -ne 0 ]; then
+  echo "ERROR $TNAME returned $retVal"
+  exit $retVal
+fi
+
+# spot checks
+# First argument is the number of expected checks
+${SPOTCHECKS} $NUMCHECKS $LOGFILE
+
+RC=$?
+if [ $RC -ne 0 ]; then
+  echo "ERROR: Test Failed with RC=$RC"
+  exit $RC
+fi
+
+
+# Cleanup output file on pass
+if [ $CLEANUP -eq 1 ]; then
+  rm -f $LOGFILE $OUTFILE $CMDFILE $CHKFILE
+fi
+
+wait
+echo "PASS"
+exit 0
